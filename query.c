b7b37875b6507c09e004c44d9ee9e23c6a43f355
stephenpsq <37071362+span6484@users.noreply.github.com>
2022-04-14 11:21:20 +0800

    ADD STH TO QUERY
    PLAN TO STORE ALL UNKNOWNS INTO THE Array

--- query.c
+++ query.c
@@ -14,7 +14,7 @@
 struct QueryRep {
 	Reln    rel;       // need to remember Relation info 检索
 	Bits    known;     // the hash value from MAH
-	Bits    unknown;   // the unknown bits from MAH
+	Bits    unknown;   // the unknown bits from MAH,unknown 可以理解为一个flag
 	PageID  curpage;   // current page in scan  当前bucket pageiD， 这个不改变
 	int     is_ovflow; // are we in the overflow pages?
 	Offset  curtup;    // offset of current tuple within page           //当前tuple在哪,tuple的位置, 通常为字符串索引位置,EX: 0x0001   , curpage永远不变，是dataPage
@@ -22,8 +22,8 @@ struct QueryRep {
     //添加自己的属性
     Offset curTupleIndex;
     PageID curScanPage;    //当前正在检索哪个位置, 这个是 data page or overflow page
-    Tuple tuple;
-    char* quesryString;   //检索传入的字符串
+    int nstars;
+    Bits* unknowns;
 };
 //00 -> a b ,c -> o01->d,e,f            00 是datapage， o01是 overflow page， curpage指向bucket page， 这个固定死了， 可以理解成 array的第一个， curPage指向00， curScanPage指向a,b,c
 //01 -> h, i, g -> o02 -> x, y, z -> o03 -> m,n
@@ -38,6 +38,10 @@ struct QueryRep {
 // 0 1 0 1
 // 1 1 0 0
 // 1 1 0 1
+
+int countBits();
+
+int new_pow(int i, Count depth);
 
 // known <= index <= (known | unknow)
 Query startQuery(Reln r, char *q)
@@ -56,7 +60,8 @@ Query startQuery(Reln r, char *q)
     Count nvals = nattrs(r);
     char **tuple_vals = malloc(nvals*sizeof(char *));
     Bits h[nvals];              //hash for each attribute
-    Bits hash = 0;
+    Bits known = 0;
+    Bits unknown = 0;
     assert(tuple_vals != NULL);
     // set tuple into array, ignore the , , ex: 123,?,? => ['123','?','?']
     tupleVals(q,tuple_vals);
@@ -65,32 +70,65 @@ Query startQuery(Reln r, char *q)
     ChVecItem *cv = chvec(r);
     for (i = 0; i < nvals; i ++) {
         h[i] = hash_any((unsigned char *)tuple_vals[i], strlen(tuple_vals[i]));
+        bitsString(h[i], buf);
+        printf("%s: %s\n", tuple_vals[i], buf);
     }
     for (i = 0; i < MAXCHVEC; i++) {
         a = cv[i].att;
         b = cv[i].bit;
-        printf("%s\n", *(tuple_vals+a));
-        if (bitIsSet(h[a],b) && strcmp("?", *(tuple_vals+a))) {
-            hash = setBit(hash, i);
+        if (strcmp("?", *(tuple_vals+a))) {
+            if (bitIsSet(h[a],b)) {
+                known = setBit(known, i);
+            }
         }
-        //TODO UNHASH
-//        else{
-//
-//        }
+        //if ?, straingtly set in unknown
+        else {
+            unknown = setBit(unknown,i);
+        }
     }
+    int nstars = countBits(getLower(unknown, depth(r)));
+    Bits* unknowns = malloc(sizeof(Bits) * nvals);
+    assert(unknowns != NULL);
+    for
     //FINISH-------
-//	hash = hash_any((unsigned char *)vals[0],strlen(vals[0]));
-    bitsString(hash,buf);
-    printf("%s\n", buf);
+//    bitsString(known,buf);
+//    printf("known: %s\n", buf);
+//    bitsString(unknown,buf);
+//    printf("unknown: %s\n", buf);
+//    Bits hash_lower = getLower(known | unknown, depth(r));
+//    bitsString(hash_lower,buf);
+//    printf("depth %d\n", depth(r));
+//    printf("hash_lower: %s\n", buf);
+    new->nstars = nstars;
+    new->known = known;
+    new->unknown = unknown;
+    new->rel = r;
     return new;
 }
+
+int countBits(unsigned int value)
+{
+
+    int count = 0;
+    int cmp = 1;
+    int i = 0;
+    while(i < 33) {
+        if ((cmp & value) != 0)  {
+            count += 1;
+        }
+        cmp = cmp<<1;
+        i++;
+    }
+    printf("count :%d\n", count);
+    return count;
+}
+
 
 // get next tuple during a scan
 //
 //遍历当前的page, 每次扫描的是curScanPage， curPage 是dataPage不变的， 变化的是curScanPage
 // 如果有Overflow Page，curScanPage转到了OverFlowPage上
 //如果当前Page没有， 往下一个bucket走，bucket 是page和overFlowPage
-
 Tuple getNextTuple(Query q)
 {
 	// TODO
@@ -118,7 +156,40 @@ Tuple getNextTuple(Query q)
 //
 //    }
 
+    char buf[MAXBITS+1];
+    Bits known = q->known;
+    Bits unknown = q->unknown;
+    Reln r = q->rel;
+    bitsString(known,buf);
+    printf("In getNext Tuple-----------\n");
+    printf("known: %s\n", buf);
+    bitsString(unknown,buf);
+    printf("unknown: %s\n", buf);
+    Bits hash_lower = getLower(known | unknown, depth(r));
+    bitsString(hash_lower,buf);
+    printf("depth %d\n", depth(r));
+    printf("hash_lower: %s\n", buf);
+//    Page p = getPage(dataFile(r),hash_lower);
+//    char *data = pageData(p);
+    bitsString(getLower(unknown, depth(r)),buf);
+    printf("unknown lower is: %s\n",buf);
+    printf("valid_unknown: %d\n", q->nstars);
+//    int i;
+
+//    for (i = 0; i < new_pow(2, depth(r)); i++) {
+//        p =
+//    }
 	return NULL;
+}
+
+int new_pow(int i, Count depth) {
+    int j;
+    int val = 1;
+    for(j = 0; j < depth; j++) {
+        val *= 2;
+    }
+
+    return val;
 }
 
 // clean up a QueryRep object and associated data
